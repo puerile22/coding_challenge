e1ce7ece820e28905126e524f1abe9679e32bad6
Peng Gao <puerile22@gmail.com>
2014-10-07 09:52:18 -0700

    towers of hanio complete

---
+++ towers_of_hanio.rb
@@ -0,0 +1,17 @@
+@count = 0
+def hanio(num, towers, start=0, fin=2,mid=1)
+  if num == 1
+    @count+=1
+    towers[fin] << towers[start].pop
+    puts "move disk from #{start} to #{fin}, #{towers}"
+    puts "#{@count}"
+  else
+    hanio(num-1,towers,start,mid,fin)
+    hanio(1,towers,start,fin,mid)
+    hanio(num-1,towers,mid,fin,start)
+  end
+end
+
+towers=[[5,4,3,2,1],[],[]]
+num = 5
+hanio(num,towers)
